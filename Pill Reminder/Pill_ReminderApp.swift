167397da37f13b1bc71edbdbe7ade2ace7c3d59e
Walizada300 <walizada.300@gmail.com>
2025-07-06 10:54:07 +0430

    Initial Commit

---
+++ Pill Reminder/Pill_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pill_ReminderApp.swift
+//  Pill Reminder
+//
+//  Created by RTA Media on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Pill_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pill ReminderTests/Pill_ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pill_ReminderTests.swift
+//  Pill ReminderTests
+//
+//  Created by RTA Media on 7/6/25.
+//
+
+import Testing
+@testable import Pill_Reminder
+
+struct Pill_ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
